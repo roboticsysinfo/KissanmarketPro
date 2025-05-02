674dcefcf5e1b09046270e1cf160cb5125a4c624
roboticsysinfo <roboticsysinfo@gmail.com>
2025-05-02 15:58:52 +0530

    update

--- src/components/admin/AddTermsCondition.jsx
+++ src/components/admin/AddTermsCondition.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Button } from 'react-bootstrap';
-import { fetchSiteDetails, updateTermsAndConditions } from '../../redux/slices/siteDetailsSlice'; // Corrected path
+import { fetchSiteDetails, updateTermsAndConditions } from '../../redux/slices/siteDeatilsSlice'; // Corrected path
 import toast from 'react-hot-toast';
 
 // Lazy load ReactQuill
