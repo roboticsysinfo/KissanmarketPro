754af1d6bb4b56317307e484f1becd0298b71fbf
roboticsysinfo <roboticsysinfo@gmail.com>
2025-05-06 16:20:50 +0530

    recpatch remove notfication update

--- src/components/BottomFooter.jsx
+++ src/components/BottomFooter.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const BottomFooter = () => {
+
     return (
         <div className="bottom-footer bg-color-one py-8">
             <div className="container container-lg">
                 <div className="bottom-footer__inner flex-center flex-wrap gap-16 py-16">
                     <p className="bottom-footer__text texxt-center ">
-                        Kissan Growth © 2025. All Rights Reserved,  Robotic SysInfo{" "}
+                        Kissan Growth © 2025. All Rights Reserved,  <Link to="https://roboticsysinfo.com">Robotic SysInfo</Link>{" "}
                     </p>
                 </div>
             </div>
         </div>
 
     )
+
 }
+
 
 export default BottomFooter
--- src/components/HeaderOne.jsx
+++ src/components/HeaderOne.jsx
@@ -425,12 +425,14 @@ const HeaderOne = () => {
                                 {userId ? (
                                     <>
 
-                                        <div className='notification_bell'>
-                                            <FaRegBell />
-                                            <Badge pill bg="danger" className="notificatin_badge">
-                                                0
-                                            </Badge>
-                                        </div>
+                                        <Link to="/notifications">
+                                            <div className='notification_bell'>
+                                                <FaRegBell />
+                                                <Badge pill bg="danger" className="notificatin_badge">
+                                                    0
+                                                </Badge>
+                                            </div>
+                                        </Link>
 
                                         <Link to="/account" className="flex-align gap-4 item-hover">
                                             <div class="profile_dropdown">
